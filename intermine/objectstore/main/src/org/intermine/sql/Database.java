18de5d03fc367244d948334f8b7c88a739fbcf43
Richard Smith <rnsmith100@gmail.com>
2014-06-09 18:24:07 +0100

    Close the connection pool correctly.
    Conflicts:
    	intermine/objectstore/main/src/org/intermine/sql/Database.java

--- intermine/objectstore/main/src/org/intermine/sql/Database.java
+++ intermine/objectstore/main/src/org/intermine/sql/Database.java
@@ -1,14 +1,14 @@
 package org.intermine.sql;
 
 /*
-* Copyright (C) 2002-2014 FlyMine
-*
-* This code may be freely distributed and modified under the
-* terms of the GNU Lesser General Public Licence. This should
-* be distributed with the code. See the LICENSE file for more
-* information or http://www.gnu.org/copyleft/lesser.html.
-*
-*/
+ * Copyright (C) 2002-2014 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
 
 import java.lang.ref.WeakReference;
 import java.lang.reflect.Field;
@@ -39,11 +39,11 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 /**
-* Class that represents a physical SQL database
-*
-* @author Andrew Varley
-* @author Matthew Wakeling
-*/
+ * Class that represents a physical SQL database
+ *
+ * @author Andrew Varley
+ * @author Matthew Wakeling
+ */
 public class Database implements Shutdownable
 {
     private static final Logger LOG = Logger.getLogger(Database.class);
@@ -59,19 +59,19 @@ public class Database implements Shutdownable
     // protected Map createSituations = new HashMap();
 
     /**
-* No argument constructor for testing purposes
-*/
+     * No argument constructor for testing purposes
+     */
     protected Database() {
         // empty
     }
 
 
     /**
-* Constructs a Database object from a set of properties
-*
-* @param props the properties by which this Database is configured
-* @throws ClassNotFoundException if there is a class in props that cannot be found
-*/
+     * Constructs a Database object from a set of properties
+     *
+     * @param props the properties by which this Database is configured
+     * @throws ClassNotFoundException if there is a class in props that cannot be found
+     */
     protected Database(Properties props) throws ClassNotFoundException {
         settings = props;
 
@@ -128,7 +128,7 @@ public class Database implements Shutdownable
 
     private void renameProperty(Properties props, String origName, String newName) {
         if (props.containsKey(origName)) {
-            props.put(newName, props.get(origName));
+            props.put(newName,  props.get(origName));
             props.remove(origName);
         }
     }
@@ -140,20 +140,20 @@ public class Database implements Shutdownable
     }
 
     /**
-* Gets the DataSource object for this Database
-*
-* @return the datasource for this Database
-*/
+     * Gets the DataSource object for this Database
+     *
+     * @return the datasource for this Database
+     */
     public DataSource getDataSource() {
         return datasource;
     }
 
     /**
-* Gets a Connection to this Database
-*
-* @return a Connection to this Database
-* @throws SQLException if there is a problem in the underlying database
-*/
+     * Gets a Connection to this Database
+     *
+     * @return a Connection to this Database
+     * @throws SQLException if there is a problem in the underlying database
+     */
     public Connection getConnection() throws SQLException {
         Connection retval;
         if (datasource == null) {
@@ -165,42 +165,46 @@ public class Database implements Shutdownable
             throw new RuntimeException("can't open datasource for " + this, e);
         }
         /*
-Exception e = new Exception();
-e.fillInStackTrace();
-StringWriter message = new StringWriter();
-PrintWriter pw = new PrintWriter(message);
-e.printStackTrace(pw);
-pw.close();
-String createSituation = message.toString();
-int index = createSituation.indexOf("at junit.framework.TestCase.runBare");
-createSituation = (index < 0 ? createSituation : createSituation.substring(0, index));
-createSituations.put(retval, createSituation);
-*/
+        Exception e = new Exception();
+        e.fillInStackTrace();
+        StringWriter message = new StringWriter();
+        PrintWriter pw = new PrintWriter(message);
+        e.printStackTrace(pw);
+        pw.close();
+        String createSituation = message.toString();
+        int index = createSituation.indexOf("at junit.framework.TestCase.runBare");
+        createSituation = (index < 0 ? createSituation : createSituation.substring(0, index));
+        createSituations.put(retval, createSituation);
+        */
         return retval;
     }
 
     /**
-* Logs stuff
-*/
+     * Logs stuff
+     */
     public void shutdown() {
         /*int totalConnections = 0;
-int activeConnections = 0;
-Iterator iter = createSituations.entrySet().iterator();
-while (iter.hasNext()) {
-Map.Entry entry = (Map.Entry) iter.next();
-Connection con = (Connection) entry.getKey();
-String sit = (String) entry.getValue();
-String conDesc = con.toString();
-if (conDesc.indexOf("Pooled connection wrapping physical connection null") == -1) {
-LOG.info("Possibly active connection for Database " + getURL() + "(" + toString()
-+ "), connection: " + con + ", createSituation: " + sit);
-activeConnections++;
-}
-totalConnections++;
-}
-LOG.info("Database " + getURL() + "(" + toString() + ") has " + totalConnections
-+ " connections, of which " + activeConnections + " are active");*/
-        if (datasource instanceof org.postgresql.ds.PGPoolingDataSource) {
+        int activeConnections = 0;
+        Iterator iter = createSituations.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry entry = (Map.Entry) iter.next();
+            Connection con = (Connection) entry.getKey();
+            String sit = (String) entry.getValue();
+            String conDesc = con.toString();
+            if (conDesc.indexOf("Pooled connection wrapping physical connection null") == -1) {
+                LOG.info("Possibly active connection for Database " + getURL() + "(" + toString()
+                        + "), connection: " + con + ", createSituation: " + sit);
+                activeConnections++;
+            }
+            totalConnections++;
+        }
+        LOG.info("Database " + getURL() + "(" + toString() + ") has " + totalConnections
+                + " connections, of which " + activeConnections + " are active");*/
+        if (datasource instanceof com.zaxxer.hikari.HikariDataSource) {
+            LOG.info("Shutdown - Closing datasource for Database " + getURL() + "(" + toString()
+                    + ") with ClassLoader " + getClass().getClassLoader());
+            ((com.zaxxer.hikari.HikariDataSource) datasource).close();
+        } else if (datasource instanceof org.postgresql.ds.PGPoolingDataSource) {
             LOG.info("Shutdown - Closing datasource for Database " + getURL() + "(" + toString()
                     + ") with ClassLoader " + getClass().getClassLoader());
             ((org.postgresql.ds.PGPoolingDataSource) datasource).close();
@@ -216,12 +220,16 @@ LOG.info("Database " + getURL() + "(" + toString() + ") has " + totalConnections
     }
 
     /**
-* {@inheritDoc}
-*/
+     * {@inheritDoc}
+     */
     @Override
     public void finalize() throws Throwable {
         super.finalize();
-        if (datasource instanceof org.postgresql.ds.PGPoolingDataSource) {
+        if (datasource instanceof com.zaxxer.hikari.HikariDataSource) {
+            LOG.info("Finalise - Closing datasource for Database " + getURL() + "(" + toString()
+                    + ") with ClassLoader " + getClass().getClassLoader());
+            ((com.zaxxer.hikari.HikariDataSource) datasource).close();
+        } else if (datasource instanceof org.postgresql.ds.PGPoolingDataSource) {
             LOG.info("Finalise - Closing datasource for Database " + getURL() + "(" + toString()
                     + ") with ClassLoader " + getClass().getClassLoader());
             ((org.postgresql.ds.PGPoolingDataSource) datasource).close();
@@ -237,46 +245,46 @@ LOG.info("Database " + getURL() + "(" + toString() + ") has " + totalConnections
     }
 
     /**
-* Gets the platform of this Database
-*
-* @return the datasource for this Database
-*/
+     * Gets the platform of this Database
+     *
+     * @return the datasource for this Database
+     */
     public String getPlatform() {
         return platform;
     }
 
     /**
-* Gets the driver this Database
-*
-* @return the driver for this Database
-*/
+     * Gets the driver this Database
+     *
+     * @return the driver for this Database
+     */
     public String getDriver() {
         return driver;
     }
 
     /**
-* Gets the username for this Database
-*
-* @return the username for this Database
-*/
+     * Gets the username for this Database
+     *
+     * @return the username for this Database
+     */
     public String getUser() {
         return (String) settings.get("datasource.user");
     }
 
     /**
-* Gets the password for this Database
-*
-* @return the password for this Database
-*/
+     * Gets the password for this Database
+     *
+     * @return the password for this Database
+     */
     public String getPassword() {
         return (String) settings.get("datasource.password");
     }
 
     /**
-* Gets the URL from this database
-*
-* @return the URL for this database
-*/
+     * Gets the URL from this database
+     *
+     * @return the URL for this database
+     */
     public String getURL() {
 
         StringBuffer urlBuffer = new StringBuffer();
@@ -288,33 +296,33 @@ LOG.info("Database " + getURL() + "(" + toString() + ") has " + totalConnections
         urlBuffer.append("/" + (String) settings.get("datasource.databaseName"));
         String url = urlBuffer.toString();
 
-// if (platform.equalsIgnoreCase("oracle")) {
-// //jdbc:oracle:thin:@oracle.flymine.org:1521:XE
-// url = "jdbc:" + platform.toLowerCase() + ":" // oracle
-// + (String) settings.get("datasource.driverType") + ":@" // thin
-// + (String) settings.get("datasource.serverName") + ":" // oracle.flymine.org
-// + (String) settings.get("datasource.portNumber") + ":" // 1521
-// + (String) settings.get("datasource.databaseName"); // XE
-// }
+//        if (platform.equalsIgnoreCase("oracle")) {
+//            //jdbc:oracle:thin:@oracle.flymine.org:1521:XE
+//            url = "jdbc:" + platform.toLowerCase() + ":"                    // oracle
+//                  + (String) settings.get("datasource.driverType") + ":@"   // thin
+//                  + (String) settings.get("datasource.serverName") + ":"    // oracle.flymine.org
+//                  + (String) settings.get("datasource.portNumber") + ":"    // 1521
+//                  + (String) settings.get("datasource.databaseName");       // XE
+//        }
         return url;
     }
 
     /**
-* Gets the database name only, not the full URL.
-* @return the database name
-*/
+     * Gets the database name only, not the full URL.
+     * @return the database name
+     */
     public String getName() {
         return (String) settings.get("datasource.databaseName");
     }
 
     /**
-* Configures a datasource from a Properties object
-*
-* @param props the properties for configuring the Database
-* @throws ClassNotFoundException if the class given in the properties file cannot be found
-* @throws IllegalArgumentException if the configuration properties are empty
-* @throws NullPointerException if props is null
-*/
+     * Configures a datasource from a Properties object
+     *
+     * @param props the properties for configuring the Database
+     * @throws ClassNotFoundException if the class given in the properties file cannot be found
+     * @throws IllegalArgumentException if the configuration properties are empty
+     * @throws NullPointerException if props is null
+     */
     protected void configure(Properties props) throws ClassNotFoundException {
         if (props == null) {
             throw new NullPointerException("Props cannot be null");
@@ -431,18 +439,18 @@ LOG.info("Database " + getURL() + "(" + toString() + ") has " + totalConnections
     }
 
     /**
-* Return the SQL type used to store objects of the given Class. eg. return "double precision"
-* for Double.class
-* @param c the Class representing the java type
-* @return the SQL type
-*/
+     * Return the SQL type used to store objects of the given Class.  eg. return "double precision"
+     * for Double.class
+     * @param c the Class representing the java type
+     * @return the SQL type
+     */
     public String getColumnTypeString(Class<?> c) {
         return POSTGRESQL_TYPE_STRING_MAP.get(c);
     }
 
     /**
-* {@inheritDoc}
-*/
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return "" + settings + " " + driver + " " + platform;
@@ -456,13 +464,13 @@ LOG.info("Database " + getURL() + "(" + toString() + ") has " + totalConnections
     private int threadNo = 1;
 
     /**
-* Executes an SQL statement on the database in a separate thread. A certain number of worker
-* threads controlled by the "parallel" property will operate simultaneously.
-*
-* @param sql an SQL string.
-* @throws SQLException if an error has been reported by a previous operation - however, this
-* does not cancel the current operation.
-*/
+     * Executes an SQL statement on the database in a separate thread. A certain number of worker
+     * threads controlled by the "parallel" property will operate simultaneously.
+     *
+     * @param sql an SQL string.
+     * @throws SQLException if an error has been reported by a previous operation - however, this
+     * does not cancel the current operation.
+     */
     public void executeSqlInParallel(String sql) throws SQLException {
         SqlJob job = new SqlJob(sql);
         synchronized (this) {
@@ -497,11 +505,11 @@ LOG.info("Database " + getURL() + "(" + toString() + ") has " + totalConnections
     }
 
     /**
-* Blocks until all the current pending jobs are finished. This will not block new jobs from
-* arriving, and those new jobs do not need to be finished for this method to return.
-*
-* @throws SQLException if an error has been reported by a previous operation
-*/
+     * Blocks until all the current pending jobs are finished. This will not block new jobs from
+     * arriving, and those new jobs do not need to be finished for this method to return.
+     *
+     * @throws SQLException if an error has been reported by a previous operation
+     */
     public void waitForCurrentJobs() throws SQLException {
         Waiter waiter;
         synchronized (this) {
@@ -534,8 +542,8 @@ LOG.info("Database " + getURL() + "(" + toString() + ") has " + totalConnections
     }
 
     /**
-* Wrap String, so that we have default equals and hashcode.
-*/
+     * Wrap String, so that we have default equals and hashcode.
+     */
     private class SqlJob
     {
         String sql;
@@ -550,17 +558,17 @@ LOG.info("Database " + getURL() + "(" + toString() + ") has " + totalConnections
     }
 
     /**
-* Worker thread.
-*/
+     * Worker thread.
+     */
     private class Worker implements Runnable
     {
         private int threadNo;
 
         /**
-* Create a new worker thread object.
-*
-* @param threadNo the thread index of this thread
-*/
+         * Create a new worker thread object.
+         *
+         * @param threadNo the thread index of this thread
+         */
         public Worker(int threadNo) {
             this.threadNo = threadNo;
         }
@@ -599,8 +607,8 @@ LOG.info("Database " + getURL() + "(" + toString() + ") has " + totalConnections
     }
 
     /**
-* An object to facilitate waiting for a set of operations to be finished.
-*/
+     * An object to facilitate waiting for a set of operations to be finished.
+     */
     private class Waiter
     {
         private Set<SqlJob> waitingOn;
