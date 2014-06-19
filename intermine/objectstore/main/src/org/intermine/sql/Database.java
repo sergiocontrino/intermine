367a29c0e9b1987995bd122a33b49a8ba3fb003b
sergio contrino <contrino.it@gmail.com>
2014-06-19 10:44:00 +0100

    corrects paths

--- intermine/objectstore/main/src/org/intermine/sql/Database.java
+++ intermine/objectstore/main/src/org/intermine/sql/Database.java
@@ -29,12 +29,12 @@ import java.util.concurrent.ArrayBlockingQueue;
 import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
-import org.intermine.metadata.StringUtil;
+//import org.intermine.metadata.StringUtil;
 import org.intermine.util.PropertiesUtil;
 import org.intermine.util.ShutdownHook;
 import org.intermine.util.Shutdownable;
 import org.postgresql.util.PSQLException;
-
+import org.intermine.util.StringUtil;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
