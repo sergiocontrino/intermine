5f9b60ff4dfbd156376be1257f865ea421d16ec3
eriksf <erik@jcvi.org>
2015-05-19 15:19:44 -0400

    [MINE-822] fixed issue of database sequences not being dropped properly on rebuild

--- intermine/objectstore/main/src/org/intermine/sql/DatabaseUtil.java
+++ intermine/objectstore/main/src/org/intermine/sql/DatabaseUtil.java
@@ -599,6 +599,18 @@ public final class DatabaseUtil
     }
 
     /**
+     * Remove the view from the database given.
+     *
+     * @param con the Connection to the database
+     * @param view the view to remove
+     * @throws SQLException if an error occurs in the underlying database
+     */
+    public static void removeView(Connection con, String view) throws SQLException {
+        LOG.info("Dropping view " + view);
+        con.createStatement().execute("DROP VIEW " + view);
+    }
+
+    /**
      * Creates a table name for a class descriptor
      *
      * @param cld ClassDescriptor
