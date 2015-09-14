768286813f2e64cb32d58ef4e30439ba32ed883c
sergio contrino <contrino.it@gmail.com>
2015-09-14 16:21:17 +0100

    removes detection method (can now be null) (MINE-1015)

--- bio/webapp/src/org/intermine/bio/web/displayer/CytoscapeNetworkDisplayer.java
+++ bio/webapp/src/org/intermine/bio/web/displayer/CytoscapeNetworkDisplayer.java
@@ -153,7 +153,7 @@ public class CytoscapeNetworkDisplayer extends ReportDisplayer
                 "Gene.interactions.gene2.symbol",
                 "Gene.interactions.gene2.primaryIdentifier",
                 "Gene.interactions.details.role2",
-                "Gene.interactions.details.experiment.interactionDetectionMethods.name",
+//                "Gene.interactions.details.experiment.interactionDetectionMethods.name",
                 "Gene.interactions.details.dataSets.dataSource.name",
 //                "Gene.interactions.gene1.interactions.details.experiment.interactionDetectionMethods.name",
                 "Gene.interactions.details.experiment.publication.title",
