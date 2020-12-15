2ee06df22b31373b703562b43cb3af489f467e45
Daniela Butano <daniela@intermine.org>
2020-12-15 17:29:46 +0000

    added echo

--- config/travis/testDB.sh
+++ config/travis/testDB.sh
@@ -1,5 +1,6 @@
 echo 'tesdtDD....'
 export PGPASSWORD=postgres
 echo "${PGPASSWORD}"
+echo "update pg_hba.conf"
 echo "host all all all md5" >> pg_hba.conf
 psql -U postgres -c "create database imtest;"
