0514954ac0bf4bb31e37eec6f670293e505924f2
Daniela Butano <daniela@intermine.org>
2020-12-15 16:47:28 +0000

    another psl command to test

--- config/travis/testDB.sh
+++ config/travis/testDB.sh
@@ -1,4 +1,4 @@
 echo 'tesdtDD....'
 #export PGPASSWORD=postgres
 #echo "${PGPASSWORD}"
-psql -d postgres -c "create database imtest;"
+psql -h localhost -U postgres -d postgres -c "create database imtest;"
