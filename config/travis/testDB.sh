801a9e050c0089aaea242e0ce132595a7764c06c
Daniela Butano <daniela@intermine.org>
2020-12-15 17:34:08 +0000

    try to use trust

--- config/travis/testDB.sh
+++ config/travis/testDB.sh
@@ -1,6 +1,6 @@
 echo 'tesdtDD....'
-export PGPASSWORD=postgres
-echo "${PGPASSWORD}"
+#export PGPASSWORD=postgres
+#echo "${PGPASSWORD}"
 echo "update pg_hba.conf"
-echo "host all all all md5" >> pg_hba.conf
+echo "host all all all trust" >> pg_hba.conf
 psql -U postgres -c "create database imtest;"
